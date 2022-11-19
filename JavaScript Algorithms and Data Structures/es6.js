90680b81cd1f5cde27cf03e266bf7903ac0b3cd7
Tracy <tracy.chacon.00@gmail.com>
2022-11-19 01:45:28 -0600

    completed ES6, promises resolve/reject/then/catch states

--- JavaScript Algorithms and Data Structures/es6.js
+++ JavaScript Algorithms and Data Structures/es6.js
@@ -1,8 +1,6 @@
 // # ES6
 //ECMAScript, or ES, is a standardized version of JavaScript. because all major browsers follow this specification, the terms ECMAScript and JavaScript are interchangeable.
 
-const { subtract } = require("lodash");
-
 // Most of the JavaScript you've learned up to this point was in ES5 (ECMAScript5), which was finalized in 2009. While yo can still write programs in ES5, JavaScript is constantly evolving, and new features are released every year.
 
 //ES6, released in 2015, added many powerful new features to the language. In this course, you'll learn these new features, including arrow functions, destructuring, classes, promises, and modules.
@@ -72,6 +70,8 @@ const { subtract } = require("lodash");
 // Here the console will display the value '2', and the error that i is not defined'.
 
 // 'i' is not defined because it was not declared in teh global scopoe. It is only declared within the 'for' loop SVGPatternElement. 'printNumTwo()' returned the correct value because three different 'i' variables with unique values (0, 1, and 2) were created by the 'let keyword within the loop statement.
+
+
 
 
 
@@ -825,3 +825,117 @@ const { subtract } = require("lodash");
 
 // subtract(7, 4);
 
+
+
+
+
+// ## Create a JavaScript Promise
+
+// A promise in JavaScript is exactly what it sounds like-you use it to make a promise to do something, usually ***asynchronously***. When the task completes, you either fulfill your promise or fail to do so. 'Promise' is a constructor fucntion, so you need to use the 'new' keyword to create one. It takes a function, as its argument, with two perameters-'resolve' and 'reject'. These are methods used to determine the outcome of the promise. The syntax looks like this:
+
+// const myPromise = new Promise((resolve, reject) => {});
+
+// Create a new promise called 'makeServerRequest'. Pass in a function with 'resolve' and 'reject' parameters to the constructor.
+
+// const makeServerRequest = new Promise((resolve, reject) => {});
+
+
+
+
+
+
+
+// ## Complete a Promise with resolve and reject
+
+// A promise has three states: 'pending', 'fulfilled', and 'rejected'. The promise you created in the last challenge is forever stuck in the 'pending' state because yo did not add a way to complete the promise. The 'resolve' and 'reject' parameters given to the promise argument are used to do this. 'resolve' is used when you want your promise to succeed, and 'reject' is used when you want it to fail. These are methodes that take an argument, as seen below.
+
+// const myPromise = new Promise((resolve, reject) => {
+//     if(condition_here) {
+//         resolve("Promise was fulfilled");
+//     } else {
+//         reject("Promise was rejected");
+//     }
+// });
+
+// The example above uses strings for the argument of these funcitons, but it can really be anything. Often, it might be an object, that you would use data from, to put on your website or elsewhere.
+
+//  Make the promise handle success and failure. If 'responseFromServer' is 'true', call the 'resolve' method to successfully  complete the promise. Pass 'resolve' a string with the value 'We got the data'. If 'responseFromServer' is 'false', use the 'reject' method instead and pass it the string: 'Data not received'.
+
+// const makeServerRequest = new Promise((resolve, reject) => {
+//     // responseFromServer represents a response from a server
+//     let resonseFromServer;
+
+//     if(responseFromServer) {
+//         // Change this line
+//         resolve("We got the data");
+//     } else {
+//         // Change this line
+//         reject("Data not received");
+//     }
+// });
+
+
+
+
+
+// ## Handle a Fulfilled Promise with then
+
+// Promises are most useful when have a process that takes an unknown amount of time in your code(i.e. something asynchronous), often a server request. When you make a server request it takes some amount of time, and after it completes you usually want to do something with the response from the server. This can be achieved by using the 'then' method. The 'then' method is executed immediately after your promise is fulfilled with 'resolve'. Here's an example:
+
+// myPromise.then(result => {
+    
+// });
+
+// 'result' comes from the argument given to the 'resolve' method.
+
+// Add the 'then' method to your promise. Use 'result' as the parameter of its callback function and log 'result' to the console.
+
+// const makeServerRequest = new Promise((resolve, reject) => {
+//     // responseFromServer is set to true to represent a successful response from a server
+//     let responseFromServer = true;
+
+//     if(responseFromServer) {
+//         resolve("We got the data");
+//     } else {
+//         reject("Data not received");
+//     }
+// });
+
+// makeServerRequest.then(result => {
+//     console.log(result);
+// });
+
+
+
+
+
+
+
+// ## Handle a Rejected Promise with catch
+
+// 'catch' is the method used when your promise has been rejected. It is executed immediately after a promise's 'reject' method is called. Heres's the syntax:
+
+// myPromise.catch(error => {});
+
+// 'error' is the argument passed in to the 'reject' method.
+
+// Add the 'catch' method to your promise. Use 'error' as the perameter of its callback function and log 'error' to the console.
+
+// const makeServerRequest = new Promise((resolve, reject) => {
+//     // responseFromServer is set to false to represent an unsuccessful response from a server
+//     let responseFromServer = false;
+
+//     if(responseFromServer) {
+//         resolve("We got the data");
+//     } else {
+//         reject("Data not received");
+//     }
+// });
+
+// makeServerRequest.then(result => {
+//     console.log(result);
+// })
+
+// makeServerRequest.catch(error => {
+//     console.log(error);
+// });
