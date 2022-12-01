617b5b493194fa4f3002be27e42f207d6700d4b8
Tracy <tracy.chacon.00@gmail.com>
2022-11-30 21:31:28 -0600

    hazards of using imperative code

--- JavaScript Algorithms and Data Structures/debugging.js
+++ JavaScript Algorithms and Data Structures/debugging.js
@@ -251,9 +251,9 @@ const { sum, first } = require("lodash");
 
 // ## Catch Off By One Errors When Using Indexing
 
-// Off by one error (sometimes called OBOE) crop up when you're trying to target a specific index of a string or array (to slice or access a segment), or when looping over the indices of them. JavaScript indexing starts at zero, not one, which means teh last index is always one less than the length of the item. If yo try to access an index equal to the length, the program may throw an "index out of range" reference error or print 'undefined'.
+// Off by one error (sometimes called OBOE) crop up when you're trying to target a specific index of a string or array (to slice or access a segment), or when looping over the indices of them. JavaScript indexing starts at zero, not one, which means the last index is always one less than the length of the item. If you try to access an index equal to the length, the program may throw an "index out of range" reference error or print 'undefined'.
 
-// When you use string or array methods that take index ranges as arguments, it helps to read the documentation and understand if tehy are inclusive (the item at the given index is part of what's returned) or not. Here are some examples of off by one errors:
+// When you use string or array methods that take index ranges as arguments, it helps to read the documentation and understand if they are inclusive (the item at the given index is part of what's returned) or not. Here are some examples of off by one errors:
 
 // let alphabet = "abcdefghijklmnopqrstuvwxyz";
 // let len = alphabet.length;
