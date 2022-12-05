9c33649fcc36f4af4680601d47b9cf3b7e8d9f3f
Tracy <tracy.chacon.00@gmail.com>
2022-12-05 00:21:25 -0600

    #12 remove elements from an array using slice instead of splice

--- JavaScript Algorithms and Data Structures/functional-programming.js
+++ JavaScript Algorithms and Data Structures/functional-programming.js
@@ -672,3 +672,28 @@ console.log(newArry);
 
 
 
+
+// ## Remove Elements from an Array Using slice Instead of splice
+
+// A common pattern while working with arrays is when you want to remove items and keep the rest of the array. JavaScript offers the 'splice' method for this, which takes arguments for the index of where to start removing items, then the number of items to remove. If the second argument is not provided, the default is to remove items through the end. However, the 'splice' method mutates the original array is called on. Here's an example:
+
+// const cities = ["Chicago", "Delhi", "Islamabad", "London", "Berline"];
+// console.log(cities.splice(3, 1));
+
+// Here 'splice' returns the string 'London' and deletes it from the cities array. 'cities' will have the value '["Chicago", "Delhi", "Islamabad", "Berlin"]'.
+
+// As we saw in the last challenge, the 'slice' method does not mutate the original array, but returns a new one which can be saved into a variable. Recall that the 'slice' method takes two arguments for the indices to begin and end the slice (the end is non-inclusive), and returns those items in a new array. Using the 'slice' method instead of 'splice' helps to avoid any array-mutating side effects.
+
+// Rewrite the function 'nonMutatingSplice' by using 'slice' instead of 'splice'. it should limit the provided 'cites' array to a length of 3, and return a new array with only the first three items.
+
+// Do not mutate the original array provided to the function.
+
+// function nonMutatingSplice(cities) {
+//     // Only change code below this line
+//     return cities.slice(0, 3);
+//     // Only change code above this line
+// }
+
+// const inputCities = ["Chicago", "Delhi", "Islamabad", "London", "Berlin"];
+// console.log(nonMutatingSplice(inputCities));
+// console.log(inputCities);
