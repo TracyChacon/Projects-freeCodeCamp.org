bc0bd5843d42363f4f676a8378e1183a1aa132d7
Tracy <tracy.chacon.00@gmail.com>
2022-12-06 01:19:14 -0600

    5-#14 complete add elements to the end of an array using concat instead of push

--- JavaScript Algorithms and Data Structures/functional-programming.js
+++ JavaScript Algorithms and Data Structures/functional-programming.js
@@ -652,9 +652,9 @@
 
 // Here's an example:
 
-const arr = ["Cat", "Dog", "Tiger", "Zebra"];
-const newArry = arr.slice(1, 3);
-console.log(newArry);
+// const arr = ["Cat", "Dog", "Tiger", "Zebra"];
+// const newArry = arr.slice(1, 3);
+// console.log(newArry);
 
 // 'newArray' would have the value '["Dog", "Tiger"].
 
@@ -730,3 +730,29 @@ console.log(newArry);
 
 
 // Add Elements to the End of an Array Using concat Instead of push 5-#14
+
+// Functional programming is all about createing and using non-mutating functions.
+
+// The last challenge introduced the 'concat' method as a way to merge arrays into a new array without mutating the original arrays. Compare 'concat' to the 'push' method. 'push' adds items to the end of the same array it is called on, which mutates that array. Here's an example:
+
+// const arr = [1, 2, 3];
+// arr.push(4, 5, 6);
+
+// console.log(arr);
+
+// 'arr' would have a modified value of '[1, 2, 3, 4, 5, 6]', which is not the functional programming way.
+
+// 'concat' offers a way to merge new items to the end of an array without any mutating side effects.
+
+// Change the 'nonMutatingPush' function so it uses 'concat' to merge 'newItem' to the end of the 'original' without mutating 'original' or 'newItem' arrays. The function should return an array.
+
+function nonMutatingPush(original, newItem) {
+    // Only change code below this line
+    return original.concat(newItem);
+    // Only change code above this line
+}
+
+const first = [1, 2, 3];
+const second = [4, 5];
+console.log(nonMutatingPush(first, second));
+
