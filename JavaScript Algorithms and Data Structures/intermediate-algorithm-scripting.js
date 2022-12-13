674a2f8b2be8ce625a841c09253378767db1773f
Tracy <tracy.chacon.00@gmail.com>
2022-12-12 21:56:39 -0600

    problem_#3js_i_algo_script seek and destroy

--- JavaScript Algorithms and Data Structures/intermediate-algorithm-scripting.js
+++ JavaScript Algorithms and Data Structures/intermediate-algorithm-scripting.js
@@ -32,35 +32,66 @@
 
 
 
-// ## Diff Two Arrays
+// ## Diff Two Arrays #2
 
 // Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. In other words, return the 'symmetric difference' of the two arrays.
 
 // Note: You can return the array with its elements in any order.
 
-function diffArray(arr1, arr2) {
+// function diffArray(arr1, arr2) {
 
-    const newArr = arr1
-        .filter(element => !arr2.includes(element))
-        .concat(arr2.filter(element => !arr1.includes(element)))
+//     const newArr = arr1
+//         .filter(element => !arr2.includes(element))
+//         .concat(arr2.filter(element => !arr1.includes(element)))
 
-    const someArr = arr1
-        .filter(element => {
-            console.log(!arr2.includes(element))
-            return !arr2.includes(element)})
-        .concat(arr2.filter(element => {
-            return !arr1.includes(element)}));
+//     const someArr = arr1
+//         .filter(element => {
+//             console.log(!arr2.includes(element))
+//             return !arr2.includes(element)})
+//         .concat(arr2.filter(element => {
+//             return !arr1.includes(element)}));
 
-    const union = [...new Set([...arr1, ...arr2])]
-    console.log(union)
+//     const union = [...new Set([...arr1, ...arr2])]
+//     console.log(union)
 
-    const intersection = arr1.filter(i => arr2.includes(i))
-    console.log(intersection)
+//     const intersection = arr1.filter(i => arr2.includes(i))
+//     console.log(intersection)
 
-    const difference = union.filter(i => !intersection.includes(i))
-    console.log(difference)
+//     const difference = union.filter(i => !intersection.includes(i))
+//     console.log(difference)
 
-    return  newArr;
-}
+//     return  newArr;
+// }
 
-console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
+// console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
+
+
+
+
+
+
+// ## Seek and Destroy
+
+// You will be provided with an initial array (the first argument in the 'destroyer' function), followed by one or more arguments. Remove all elements from the initial array that are of the same value as these arguments
+
+// Note: You have to use the 'arguments' object.
+
+// function destroyer(arr) {
+//   // store additional function arguments in 
+//   // variable 'removeArguments'
+//   const removeArguments = Array.from(arguments).slice(1);
+//   // return array without 'removeArguments' 'elements'
+//   return arr.filter(element => !removeArguments.includes(element));
+// }
+  
+
+// console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3))
+
+
+
+
+
+
+// ## Wherefore art thou
+
+// Make a funciton that look
