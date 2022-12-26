838aa4e4474c5ee2de92818d425946c8b9e604dc
Tracy <tracy.chacon.00@gmail.com>
2022-12-26 14:59:11 -0600

    #7ialgo search and replace

--- JavaScript Algorithms and Data Structures/intermediate-algorithm-scripting.js
+++ JavaScript Algorithms and Data Structures/intermediate-algorithm-scripting.js
@@ -1,6 +1,6 @@
 // # Intermediate Algorithm Scripting
 
-const { split } = require("lodash");
+const { split, lowerCase } = require("lodash");
 
 // Now that you know the basics of algorithmic thinking along with OOP and Functional Programming, test your skills with the Intermediate Algorithm Scripting challenges.
 
@@ -177,3 +177,32 @@ const { split } = require("lodash");
 
 
 
+
+
+
+// ## Search and Replace #7
+
+// Perfrom a search and replace on the sentence using the arguments provided and return the new sentence.
+
+// First argument is the sentence to perform the search and replace on.
+
+// Second argument is the word that you will be replacing(before).
+
+// Third argument is what you will be replacing the second argument with (after).
+
+// Note: Preserve the case of the first in the original word when you are replacing it. For example if you mean to replace the word 'book' with the wrod 'dog', it should be replaced as 'Dog'
+
+// function myReplace(str, before, after) {
+//     const startingIndexOfBefore = str.toLowerCase().indexOf(before.toLowerCase());
+//     const capitilizeFirstLetterAfter = after.toLowerCase().charAt(0).toUpperCase() + after.slice(1)
+//     // handle case: first letter of string variable 'before' is lower case
+//     if(str[startingIndexOfBefore] === before.toLowerCase()[0]) {
+//         return str.replace(before, after.toLowerCase());
+//     }
+//     // handle case: first letter of string variable 'before' is upper case
+//     return str.replace(before, capitilizeFirstLetterAfter);
+// };
+
+// console.log(myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped"));
+// console.log(myReplace("Let us go to the store", "store", "mall"));
+// console.log(myReplace("I think we should look up there", "up", "Down"));
