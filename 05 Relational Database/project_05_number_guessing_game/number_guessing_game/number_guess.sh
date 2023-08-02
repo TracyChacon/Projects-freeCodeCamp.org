2a8154059ec186065f9793a4978db7b1985e8fa5
Tracy Chacon <tracy.chacon.00@gmail.com>
2023-08-02 15:40:05 -0500

    feat: add prompt for username, take username as input. db allow username 22 char

--- 05 Relational Database/project_05_number_guessing_game/number_guessing_game/number_guess.sh
+++ 05 Relational Database/project_05_number_guessing_game/number_guessing_game/number_guess.sh
@@ -3,3 +3,19 @@
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
 N=$(( RANDOM %1000 + 1 ))
+
+MAIN_FUNC() {
+    echo -e "/n"
+    echo "Enter your username:"
+    read USERNAME_ENTERED
+
+    USERNAME=$($PSQL "SELECT username FROM users WHERE username='$USERNAME_ENTERED'")
+
+    echo $USERNAME
+    # if [[ -z USERNAME ]]; then
+
+    # fi
+}
+
+MAIN_FUNC
+
