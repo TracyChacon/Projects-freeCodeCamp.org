11c9679ae77286e7200047e317a38259a50f5e03
Tracy Chacon <tracy.chacon.00@gmail.com>
2023-08-03 02:26:38 -0500

    refactor: code

--- 05 Relational Database/project_05_number_guessing_game/number_guessing_game/number_guess.sh
+++ 05 Relational Database/project_05_number_guessing_game/number_guessing_game/number_guess.sh
@@ -1,47 +1,61 @@
-#!/bin/bash
-
+#! /bin/bash
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
+# USER START SECTION
+echo "Enter your username:"
+read USERNAME_ENTERED
 
-MAIN_FUNC() {
-    echo "Enter your username:"
-    read USERNAME_ENTERED
+USERNAME=$($PSQL "SELECT username FROM users WHERE username='$USERNAME_ENTERED'")
 
-    USERNAME=$($PSQL "SELECT username FROM users WHERE username='$USERNAME_ENTERED'")
+
+if [[ -n $USERNAME ]]
+then
+    GAMES_PLAYED=$($PSQL "SELECT games_played FROM users WHERE username='$USERNAME'")
+    BEST_GAME=$($PSQL "SELECT best_game FROM users WHERE username='$USERNAME'")
+
+    echo $($PSQL "UPDATE users SET games_played = 1 + $GAMES_PLAYED WHERE username='$USERNAME'")
+
+    echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+
+else
+    USERNAME=$USERNAME_ENTERED
+    echo $($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")
+
+    echo $($PSQL "UPDATE users SET games_played = 1 WHERE username='$USERNAME'")
+
+    echo "Welcome, $USERNAME! It looks like this is your first time here."
+fi
+
+
+
+# GAME SECTION
+
+SECRET_NUMBER=$(( RANDOM %1000 + 1 ))
+
+# echo "$SECRET_NUMBER is the number"
+
+echo "Guess the secret number between 1 and 1000:"
+
+until [[ $USER_INPUT == $SECRET_NUMBER ]]
+do
+    let NUMBER_OF_GUESSES+=1
     
-    if [[ -z $USERNAME ]]; then
-        echo $($PSQL "INSERT INTO users(username) values('$USERNAME_ENTERED')")
+    read USER_INPUT
 
-        echo "Welcome, $USERNAME_ENTERED! It looks like this is your first time here."
-    else
-        USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
-        GAMES_PLAYED=$($PSQL "SELECT games_played FROM stats WHERE user_id=$USER_ID")
-        BEST_GAME=$($PSQL "SELECT best_game FROM stats WHERE user_id=$USER_ID")
+    if [[ ! $USER_INPUT =~ ^[0-9]+$ ]]
+    then
+        echo "That is not an integer, guess again:"
+    elif [[ $USER_INPUT > $SECRET_NUMBER ]]
+    then
+        echo "It's lower than that, guess again:"
+    elif [[ $USER_INPUT < $SECRET_NUMBER ]]
+    then
+        echo "It's higher than that, guess again:"
+    fi 
+done
 
-        echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
-    fi
+if [[ -z $BEST_GAME ]] || [[ $NUMBER_OF_GUESSES < $BEST_GAME ]]; then
+    echo $($PSQL "UPDATE users SET best_game = $NUMBER_OF_GUESSES WHERE username='$USERNAME'")
+fi
 
-    GAME
-}
-
-GAME() {
-    USER_INPUT=0
-    N=$(( RANDOM %1000 + 1 ))
-    
-    echo "Guess the secret number between 1 and 1000:"
-
-    until [[ $USER_INPUT == $N ]]; do
-        read USER_INPUT
-
-        if [[ ! $USER_INPUT =~ ^[0-9].+$ ]]; then
-            echo "That is not an integer, guess again:"
-        elif [[ $USER_INPUT > $N ]]; then
-            echo "It's lower than that, guess again:"
-        elif [[ $USER_INPUT < $N ]]; then
-            echo "It's higher than that, guess again:"
-        fi 
-    done
-}
-
-MAIN_FUNC
-
+echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
