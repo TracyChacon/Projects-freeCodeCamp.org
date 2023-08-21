05ab56da094c3f5ed5c340811a8371873bb9cd66
Tracy Chacon <tracy.chacon.00@gmail.com>
2023-08-21 12:32:23 -0500

    chore: add body-parser dependency and npm install body-parser

--- 06 Back End Development and APIs/02 Project Headparser/index.js
+++ 06 Back End Development and APIs/02 Project Headparser/index.js
@@ -2,29 +2,36 @@
 // where your node app starts
 
 // init project
-require('dotenv').config();
-var express = require('express');
-var app = express();
+require('dotenv').config()
+var express = require('express')
+var app = express()
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
-var cors = require('cors');
-app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+var cors = require('cors')
+app.use(cors({ optionsSuccessStatus: 200 })) // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static('public'))
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
-});
+  res.sendFile(__dirname + '/views/index.html')
+})
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
-});
+app.get('/api/whoami', function (req, res) {
+  // return a JSON object with your IP address in the ipaddress key.
+  // return a JSON object with your preferred language in the language key.
+  // return a JSON object with your software in the software key.
+  res.json({
+    ipaddress: req.ip,
+    language: req.language,
+    software: req.body,
+  })
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+  console.log('Your app is listening on port ' + listener.address().port)
+})
