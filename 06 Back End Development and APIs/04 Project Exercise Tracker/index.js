70387b98b00a348fa1ee525fcc06cee39a953403
Tracy Chacon <tracy.chacon.00@gmail.com>
2023-09-21 11:14:47 -0500

    feat: dark-mode-btn js;html;css

--- 06 Back End Development and APIs/04 Project Exercise Tracker/index.js
+++ 06 Back End Development and APIs/04 Project Exercise Tracker/index.js
@@ -7,11 +7,7 @@ app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
-});
-
-
-
-
+})
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
